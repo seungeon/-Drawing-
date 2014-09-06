ce752533da42a431eabc0c92a0c0bcf06aca4187
seungeon <seungeon.k@gmail.com>
2014-09-06 20:00:27 +1000

    sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -4,3 +4,7 @@ function setup() {
   stroke(255);
   fillstyle="#000000";
 
+  line(10, 10, 90, 10); // x1, y1, x2, y2
+    rect(20, 20, 80, 50); // x, y, width, height
+    ellipse(50, 50, 50, 50); // centerX, centerY, width, height
+}
