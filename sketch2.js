6d35d5ec3cd2cae34b13532ff10f7a3a21ce37a7
seungeon <seungeon.k@gmail.com>
2014-09-06 20:17:44 +1000

    sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -2,13 +2,14 @@ function setup() {
   createCanvas(500, 420);
   background(0);
   stroke(255);
-   noFill();
- // x1, y1, x2, y2
-    ctx.fillStyle="rgb(200,0,0)"
-    ctx.fillRect(60, 20, 120, 20); // x, y, width, height
+}
 
-    ctx.fillStyle="rgb(0,0,200,0.5)";
-    ctx.fillRect(50,30,130,30);
-    ctx.fillRect(50,40,130,40);
+function draw() {
+  var ctx = document.getElementById('canvas').getContext('2d');
 
+  ctx.fillStyle = "rgb(200,0,0)";
+  ctx.fillRect(10, 10, 55, 50);
+
+  ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+  ctx.fillRect(30, 30, 55, 50);
 }
