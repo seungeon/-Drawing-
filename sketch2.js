2cb8ec24a3449580476baf153c96fe51dfb25ab7
seungeon <seungeon.k@gmail.com>
2014-09-06 20:10:13 +1000

    sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -1,9 +1,11 @@
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(500, 420);
   background(0);
   stroke(255);
-  fillstyle="#000000";
-
-  Rect(5,4,13,4)
+   noFill();
+ // x1, y1, x2, y2
+    rect(60, 20, 120, 20); // x, y, width, height
+    rect(5,3,13,3);
+    rect(5,4,13,4);
 
 }
