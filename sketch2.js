ccb02ad5cb82513ed27fb48b85f5ed8c5579f5c3
seungeon <seungeon.k@gmail.com>
2014-09-06 20:26:53 +1000

    sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -1,9 +1,12 @@
 function setup() {
   createCanvas(640, 480);
-  background(0,255,255);
+  background(0);
   stroke(255);
-  noFill();
 
+
+context.fillStyle= "#ffffff"; // sets color
+
+context.fillRect(10,10,100,50); // sets top left location points x,y and then width and height
   line(10, 10, 90, 10); // x1, y1, x2, y2
     rect(20, 20, 80, 50); // x, y, width, height
     ellipse(50, 50, 50, 50); // centerX, centerY, width, height
