7f15dda2e610ae9b102e3e43a7ff4903f22bdab7
seungeon <seungeon.k@gmail.com>
2014-09-06 20:06:30 +1000

    sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -4,7 +4,7 @@ function setup() {
   stroke(255);
   fillstyle="#000000";
 
-  line(10, 10, 90, 10); // x1, y1, x2, y2
-    rect(20, 20, 80, 50); // x, y, width, height
-    ellipse(50, 50, 50, 50); // centerX, centerY, width, height
+  context.fillStyle= "#FF8C00"
+  context.fllRect(5,4,13,4)
+  context.fllRect(4,5,5,6)
 }
