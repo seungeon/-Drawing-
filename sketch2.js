f08c5938b86f3006f3f4bc76f3cb191a3ef18602
seungeon <seungeon.k@gmail.com>
2014-09-06 20:15:29 +1000

    sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -4,8 +4,11 @@ function setup() {
   stroke(255);
    noFill();
  // x1, y1, x2, y2
-    rect(60, 20, 120, 20); // x, y, width, height
-    rect(50,30,130,30);
-    rect(50,40,130,40);
+    ctx.fillStyle="rgb(200,0,0)"
+    ctx.fillRect(60, 20, 120, 20); // x, y, width, height
+
+    ctx.fillStyle="rgb(0,0,200,0.5)";
+    ctx.fillRect(50,30,130,30);
+    ctx.fillRect(50,40,130,40);
 
 }
