468971baca063a8ea1f5ee59c03ae7f95f75ebff
seungeon <seungeon.k@gmail.com>
2014-09-06 20:11:48 +1000

    sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -5,7 +5,7 @@ function setup() {
    noFill();
  // x1, y1, x2, y2
     rect(60, 20, 120, 20); // x, y, width, height
-    rect(5,3,13,3);
-    rect(5,4,13,4);
+    rect(50,30,130,30);
+    rect(50,40,130,40);
 
 }
