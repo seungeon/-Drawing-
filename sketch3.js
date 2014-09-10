804e563e75213e9eb9ef20832ab48908517fcf5c
seungeon <seungeon.k@gmail.com>
2014-09-10 16:05:49 +1000

    index5.html

---
+++ sketch3.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(640, 480);
+  background(155);
+  stroke(155,255,139);
+
+//black
+
+    rect(6,2,12,2);
+    rect(5,3,13,3);
+    rect(5,5,9,6);
+    rect(11,5,12,7);
+    rect(5,6,5,9);
+
+}
