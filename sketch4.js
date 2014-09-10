8b2531a7f96aab7bf9c48b180bf2b0c4647b6648
seungeon <seungeon.k@gmail.com>
2014-09-10 16:37:45 +1000

    index6.html

---
+++ sketch4.js
@@ -0,0 +1,12 @@
+function drawrect() {
+	ctx.fillstyle="black";
+	ctx.fillRect(60,20,120,20);
+	ctx.fillrect(50,30,130,30);
+    ctx.fillrect(50,50,90,60);
+    ctx.fillrect(110,50,120,70);
+    ctx.fillrect(50,60,50,90);
+    ctx.fillrect(60,80,70,90);
+    ctx.fillrect(70,60,80,70);
+    ctx.fillrect(70,140,130,150);
+
+}
