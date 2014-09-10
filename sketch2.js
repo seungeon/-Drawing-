57b94c6aed2b514a033cf4ccec2eb3cde5ef8a35
seungeon <seungeon.k@gmail.com>
2014-09-10 13:44:16 +1000

    sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -1,10 +1,9 @@
 function setup() {
   createCanvas(640, 480);
-  background(0,255,255);
+  background(0);
   stroke(255);
 
-  var r1=new Rect(2,10,50,50,"red");
-  var s1=new Rect(60,10,50,50,"blue");
-  var oval1=new oval(200,30,20,2.0,1.0,"teal");
+Rect(60,10,50,50,"blue");
+ 
    
 }
