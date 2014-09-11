ac023011c160b6ab9ec225458f297efca82324f4
jungbiho <wjdqlgh@naver.com>
2014-09-11 14:43:24 +0900

    sketch.js

--- sketch.js
+++ sketch.js
@@ -2,9 +2,11 @@ function setup() {
   createCanvas(640, 480);
   background(0,255,255);
 
-  stroke(255);
-  noFill();
+  stroke(255,0,0);
+
   line(4, 10, 90, 10);
+
+  fill(0)
     rect(40, 20, 80, 150);
     ellipse(90, 40, 150, 150);
 }
